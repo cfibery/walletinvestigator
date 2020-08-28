8065db18cc2031911bd7778d55012c9440d55b64
cfiber <cfiber@protonmail.com>
2020-08-28 01:10:39 -0400

    update axios config

--- server.js
+++ server.js
@@ -14,7 +14,9 @@ const blacklist = ['0x0000000000000000000000000000000000000000'];
 const topSearches = {};
 
 const axiosInstance = axios.create({
+  withCredentials: true,
   headers: {
+    'X-Requested-With': 'XMLHttpRequest',
     'User-Agent':
       'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.125 Safari/537.36',
   },
